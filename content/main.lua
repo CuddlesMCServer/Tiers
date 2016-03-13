18c269b582a5380bb2c28b955258b7b4924ab619
Lord-Cuddles <georgeburton666@hotmail.com>
2016-03-13 13:43:59 +0000

    Update main.lua
    + Added tier creation
    + Added tier rank editing
    + Added tier slot clearing on an admin scale

--- content/main.lua
+++ content/main.lua
@@ -10,12 +10,14 @@ local Tiers = lukkit.addPlugin("Tiers", "v1.0-beta",
                 plugin.config.setDefault("config.slots", 3)
                 
                 -- Enable certain tier sets
+                plugin.config.set("config.unranked.enable", true)
                 plugin.config.setDefault("config.tier1.enable", true)
                 plugin.config.setDefault("config.tier2.enable", false)
                 plugin.config.setDefault("config.tier3.enable", false)
                 plugin.config.setDefault("config.tier4.enable", false)
                 
                 -- These are the names of each rank
+                plugin.config.setDefault("config.unranked.name", "Default")
                 plugin.config.setDefault("config.tier1.name", "Tier1")
                 plugin.config.setDefault("config.tier2.name", "Tier2")
                 plugin.config.setDefault("config.tier3.name", "Tier3")
@@ -41,17 +43,18 @@ local Tiers = lukkit.addPlugin("Tiers", "v1.0-beta",
                 elseif plugin.config.get("config.theme") == "seasonal" then
                     local m = tonumber( os.date("%m") )
                     local md = os.date("%m-%d")
+                    
                     if md == "01-04" and plugin.config.get("config.eastereggs") == true then
-                        t = { "§5", "§d", "§f" }
-                        plugin.print("Happy Easter!")
+                        t = { "§0", "§8", "§7" }
+                        plugin.print("April Fools!")
                     elseif md == "25-12" and plugin.config.get("config.eastereggs") == true then
-                        t = { "§c", "§a", "§f" }
+                        t = { "§2", "§c", "§a" }
                         plugin.print("Merry Christmas!")
                     elseif md == "01-08" and plugin.config.get("config.eastereggs") == true then
-                        t = { "§4", "§6", "§e" }
+                        t = { "§3", "§2", "§6" }
                         plugin.print("It is Lord_Cuddles' birthday today!")
                     elseif md == "09-05" and plugin.config.get("config.eastereggs") == true then
-                        t = { "§9", "§3", "§b" }
+                        t = { "§8", "§3", "§d" }
                         plugin.print("It is PineLily's birthday today!")
                     elseif m >= 1 and m < 4 then
                         t = { "§9", "§b", "§f" }
@@ -208,7 +211,7 @@ local Tiers = lukkit.addPlugin("Tiers", "v1.0-beta",
                                             plugin.config.set( tuuid..".me.tier", set.tier)
                                             plugin.config.set( tuuid..".me.id", args[2])
                                             plugin.config.set( tuuid..".me.pname", sender:getName())
-                                            plugin.config.set( tuuid..".me.puuid", sender:getName())
+                                            plugin.config.set( tuuid..".me.puuid", sender:getUniqueId():toString())
                                             plugin.config.save()
                                             local tr = plugin.config.get("config.tier"..set.tier..".name")
                                             server:dispatchCommand(server:getConsoleSender(), "pex user "..tname.." group add "..tr )
@@ -243,5 +246,168 @@ local Tiers = lukkit.addPlugin("Tiers", "v1.0-beta",
             end
         )
         
+        plugin.addCommand("tieradmin", "Administration command for Tiers", "/tieradmin",
+            function(sender, args)
+                if sender:hasPermission("tier.command.admin") == true then
+                    if args[1] == "create" then
+                        if args[2] and args[3] then
+                            local offline = server:getOfflinePlayer(args[2])
+                            local uuid = offline:getUniqueId():toString()
+                            if plugin.config.get("config.tier"..args[3]..".enable") == true then
+                                plugin.config.setDefault(uuid..".sets", 0)
+                                plugin.config.save()
+                                local id = plugin.config.get(uuid..".sets") + 1
+                                local set = {}
+                                set.tier = tonumber(args[3])
+                                set.slots = plugin.config.get("config.slots")
+                                if type( tonumber(args[4]) ) == "number" then
+                                    if tonumber(args[4]) > 0 then
+                                        set.slots = tonumber(args[4])
+                                    end
+                                end
+                                set.exists = true
+                                set.slotn = {}
+                                set.slotu = {}
+                                saveset(uuid, id, set)
+                                plugin.config.set(uuid..".sets", id)
+                                plugin.config.save()
+                                sender:sendMessage(t[2].."Success: Created a set of "..t[3].."Tier "..set.tier..t[2].." for "..t[3]..offline:getName() )
+                                if offline:isOnline() then
+                                    offline:sendMessage(t[2].."Success: You have a new set of "..t[3].."Tier "..set.tier..t[2].." with "..set.slots.." slots")
+                                end
+                            else
+                                if args[3] == "0" then
+                                    sender:sendMessage(t[2].."Error: This parameter is used to nullify existing tiers.")
+                                else
+                                    sender:sendMessage(t[2].."Error: Unknown tier "..t[3]..args[3]..t[2]..". Try a number above 0")
+                                end
+                            end
+                        else
+                            sender:sendMessage(t[2].."Error: /tieradmin create {owner} {tier} [slots]")
+                            sender:sendMessage(t[3].."§oCreate a new set of {tier} for {owner}")
+                        end
+                    elseif args[1] == "tier" then
+                        if args[2] and args[3] and args[4] then
+                            local offline = server:getOfflinePlayer(args[2])
+                            args[3] = tonumber(args[3])
+                            local uuid = offline:getUniqueId():toString()
+                            local unrank = false
+                            if args[4] == "0" then
+                                unrank = true
+                            end
+                            args[4] = tonumber(args[4])
+                            if type(args[3]) == "nil" then
+                                sender:sendMessage(t[2].."Error: You must specify a number for {id}")
+                                return
+                            end
+                            if type(args[4]) == "nil" and unrank == false then
+                                sender:sendMessage(t[2].."Error: You must specify a number for {tier}")
+                                return
+                            end
+                            local ns = "tier"..args[4]
+                            if unrank == true then ns = "unranked" end
+                            if plugin.config.get("config."..ns..".enable") == true then
+                                if plugin.config.get(uuid.."."..args[3]..".tier") == args[4] and unrank == false then
+                                    sender:sendMessage(t[2].."Error: This tier is already Tier "..args[4])
+                                else
+                                    local set = loadset(uuid, args[3])
+                                    if set.tier > 0 then
+                                        for i = 1, #set.slotu do
+                                            server:dispatchCommand(server:getConsoleSender(), "pex user "..set.slotu[i].." group remove "..plugin.config.get("config.tier"..set.tier..".name"))
+                                        end
+                                    end
+                                    local oldtier = set.tier
+                                    if unrank == true then
+                                        set.tier = 0
+                                    else
+                                        set.tier = args[4]
+                                        for i = 1, #set.slotu do
+                                            server:dispatchCommand(server:getConsoleSender(), "pex user "..set.slotu[i].." group add "..plugin.config.get("config.tier"..set.tier..".name"))
+                                        end
+                                    end
+                                    saveset( uuid, args[3], set )
+                                    if unrank == true then
+                                        sender:sendMessage(t[2].."Success: Tier set has been suspended!")
+                                    else
+                                        sender:sendMessage(t[2].."Success: Changed from Tier "..oldtier.." to Tier "..args[4])
+                                    end
+                                    if offline:isOnline() == true then
+                                        if unrank == true then
+                                            offline:sendMessage(t[2].."Information: Your set "..args[3].." was marked as VOID by "..sender:getName())
+                                        else
+                                            offline:sendMessage(t[2].."Information: Your set "..args[3].." had its tier set at "..args[4])
+                                        end
+                                    end
+                                end
+                            else
+                                sender:sendMessage(t[2].."Error: Tier "..args[4].." is disabled")
+                            end
+                        else
+                            sender:sendMessage(t[2].."Error: /tieradmin tier {owner} {id} {tier}")
+                            sender:sendMessage(t[3].."§oSets the tier of the set. Set {tier} to 0 to unrank")
+                        end
+                    elseif args[1] == "edit" then
+                        if args[2] and args[3] and args[4] and args[5] then
+                            local offline = server:getOfflinePlayer(args[2])
+                            local ouuid = offline:getUniqueId():toString()
+                            local oname = offline:getName()
+                            args[3] = tonumber(args[3])
+                            if type(args[3]) == "nil" then
+                                sender:sendMessage(t[2].."Error: You must specify a number for {id}")
+                                return
+                            end
+                            if type(args[4]) == "nil" then
+                                sender:sendMessage(t[2].."Error: You must specify a number for {slot}")
+                                return
+                            end
+                            args[4] = tonumber(args[4])
+                            local set = loadset(ouuid, args[3])
+                            if args[5] == "clear" then
+                                if set.exists == true then
+                                    if set.slotn[args[4]] then
+                                        local old = server:getOfflinePlayer(set.slotn[args[4]])
+                                        local olduuid = old:getUniqueId():toString()
+                                        plugin.config.clear(ouuid.."."..args[3]..".slot."..args[4])
+                                        plugin.config.clear(ouuid.."."..args[3]..".slot."..args[4])
+                                        plugin.config.clear(olduuid..".me.tier")
+                                        plugin.config.clear(olduuid..".me.id")
+                                        plugin.config.clear(olduuid..".me.pname")
+                                        plugin.config.clear(olduuid..".me.puuid")
+                                        plugin.config.save()
+                                        server:dispatchCommand(server:getConsoleSender(), "pex user "..old:getName().." group remove "..plugin.config.get("config.tier"..set.tier..".name"))
+                                        sender:sendMessage(t[2].."Success: Removed "..old:getName().." from "..oname.."'s set")
+                                        if old:isOnline() then
+                                            old:sendMessage(t[2].."Information: "..sender:getName().." removed you from "..oname.."'s set")
+                                        end
+                                        if offline:isOnline() then
+                                            offline:sendMessage(t[2].."Information: "..sender:getName().." removed "..old:getName().." from your set")
+                                        end
+                                    else
+                                        sender:sendMessage(t[2].."Error: That slot is not occupied")
+                                    end
+                                else
+                                    sender:sendMessage(t[2].."Error: That tier set does not exist")
+                                end
+                            else
+                                local toffline = server:getOfflinePlayer(args[5])
+                                local tuuid = toffline:getUniqueId():toString()
+                                local tname = toffline:getName()
+                            end
+                        else
+                            sender:sendMessage(t[2].."Error: /tieradmin edit {owner} {id} {slot} [{target}:clear]")
+                        end
+                    else    
+                        sender:sendMessage(t[1].."==========[ §cTier Command - Admin "..t[1].."]==========")
+                        sender:sendMessage(t[2].."Version: "..t[3]..plugin.version)
+                        sender:sendMessage(t[2].."/tieradmin create {owner} {tier} [slots]")
+                        sender:sendMessage(t[2].."/tieradmin tier {owner} {id} {tier}")
+                        sender:sendMessage(t[2].."/tieradmin edit {owner} {id} {slot} [{username}:clear]")
+                        sender:sendMessage(t[2].."/tieradmin immune {player} [true:false]")
+                    end
+                else
+                    sender:sendMessage(t[2].."Error: You need permission "..t[3].."tier.command.admin"..t[2].." to run that")
+                end
+            end
+        )
     end
-)
+)   
